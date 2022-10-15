1ed8050832efe3eaa042f9d1cc7daa9da5b2f577
andriusezerskis <andrius.ezerskis@ulb.be>
2022-10-15 17:29:17 +0200

    include changes

--- base_etudiants/utils.cpp
+++ base_etudiants/utils.cpp
@@ -2,8 +2,8 @@
 
 #include <unistd.h>
 
-#include "query.h"
-#include "student.h"
+#include "query.hpp"
+#include "student.hpp"
 
 size_t safe_read(int fd, void* buffer, size_t nbytes) {
   ssize_t nbytes_read = read(fd, buffer, nbytes);
