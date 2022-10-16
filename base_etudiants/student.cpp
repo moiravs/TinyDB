7407c7fa4d5be8566c77cae7b87a905d753b2ad8
andriusezerskis <andrius.ezerskis@ulb.be>
2022-10-16 10:55:08 +0200

    student_equals sans erreurs au make

--- base_etudiants/student.cpp
+++ base_etudiants/student.cpp
@@ -16,8 +16,8 @@ void student_to_str(char *buffer, student_t *s)
 
 int student_equals(student_t *s1, student_t *s2)
 {
-  return (s1->id == s2->id && s1->fname == s2->fname && 
-    s1->lname == s2->lname && s1->section == s2->section &&
+  return (s1->id == s2->id && (strcmp(s1->fname, s2->fname) == 0) && 
+    (strcmp(s1->lname, s2->lname) == 0) && (strcmp(s1->section, s2->section) == 0) &&
     s1->birthdate.tm_year == s2->birthdate.tm_year &&
     s1->birthdate.tm_mon == s2->birthdate.tm_mon && 
     s1->birthdate.tm_mday == s2->birthdate.tm_mday);
