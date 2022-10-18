cb915e3122b21b00629fab975ea3be002fc56c6b
22NS22 <itsenes01@gmail.com>
2022-10-18 14:42:35 +0200

    testtttt

--- base_etudiants/parsing.cpp
+++ base_etudiants/parsing.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 /*qsfsqfdsfqsfsqf
 qsdfsdqfsq
-fqsdfqsfdqlksfqs*/
+fqsdfqsfdqlksfqs
+ANDRI
+jen ai marre*/
 #include "student.hpp"
 
 bool parse_update(char* query, char* field_filter, char* value_filter, char* field_to_update, char* update_value) {
