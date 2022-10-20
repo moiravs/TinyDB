d114d949905cc2a8773a90b3de9de83bc28aa0b9
andriusezerskis <andrius.ezerskis@ulb.be>
2022-10-20 23:43:34 +0200

    fait select et delete dans la même fonction (jsuis trop smart)

--- base_etudiants/query.cpp
+++ base_etudiants/query.cpp
@@ -35,5 +35,67 @@ void query_list_upsize(query_result_t *result){
     result->students = (student_t*)malloc(result->psize);
     memcpy(result->students, old_data, old_psize);
   }
+}
 
+void query_select_and_delete(database_t *db, query_result_t *queryResult, char *fieldFilter, char *value_str, char *value, student_t *s, char *date_str, const char *queryKey){
+  for (size_t i = 0; i < db->lsize; i++)
+    {
+      *s = db->data[i];
+      if (strcmp(fieldFilter, "id") == 0)
+      {
+        sprintf(value_str, "%u", s->id); // convertir le id (unsigned) à un char* pour la comparaison
+        if (strcmp(value_str, value) == 0)
+        {
+          query_result_add(queryResult, *s);
+          if (strcmp(queryKey, "delete")){
+            db_delete(db, i);
+          }
+        }
+      }
+      else if (strcmp(fieldFilter, "fname") == 0)
+      {
+        if (strcmp(s->fname, value) == 0)
+        {
+          query_result_add(queryResult, *s);
+          if (strcmp(queryKey, "delete")){
+            db_delete(db, i);
+          }
+        }
+      }
+      else if (strcmp(fieldFilter, "lname") == 0)
+      {
+        if (strcmp(s->lname, value) == 0)
+        {
+          query_result_add(queryResult, *s);
+          if (strcmp(queryKey, "delete")){
+            db_delete(db, i);
+          }
+        }
+      }
+      else if (strcmp(fieldFilter, "section") == 0)
+      {
+        if (strcmp(s->section, value) == 0)
+        {
+          query_result_add(queryResult, *s);
+          if (strcmp(queryKey, "delete")){
+            db_delete(db, i);
+          }
+        }
+      }
+      else if (strcmp(fieldFilter, "birthdate") == 0)
+      {
+        strftime(date_str, 32, "%d/%B/%Y", &s->birthdate);
+        if (strcmp(date_str, value) == 0)
+        {
+          query_result_add(queryResult, *s);
+          if (strcmp(queryKey, "delete")){
+            db_delete(db, i);
+          }
+        }
+      }
+      else {
+        std::cout << "An error has occurred during the select query : bad filter." << std::endl;
+        //break;
+      }
+    }
 }
