3cdcfdfa8806a320a28b4f03f561fbe2a4cc2f6d
emily <moira.vanderslagmolen@ulb.be>
2022-11-01 13:47:51 +0100

    process comment done

--- base_etudiants/process.hpp
+++ base_etudiants/process.hpp
@@ -4,7 +4,23 @@ extern database_t *db;
 extern const char *db_path;
 extern int fdInsert[2], fdSelect[2], fdUpdate[2], fdDelete[2], fdResponse[2];
 
+/**
+ * @brief  Process who treats the select query
+ * @retval None
+ */
 void process_select();
+/**
+ * @brief  Process who treats the insert query
+ * @retval None
+ */
 void process_insert();
+/**
+ * @brief  Process who treats the delete query
+ * @retval None
+ */
 void process_delete();
+/**
+ * @brief  Process who treats the update query
+ * @retval None
+ */
 void process_update();
