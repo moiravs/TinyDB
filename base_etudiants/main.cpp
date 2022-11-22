ac83043f053ff2e84d331a63ab528d46631f4d90
emily <moira.vanderslagmolen@ulb.be>
2022-11-22 17:02:35 +0100

    vbfhdj

--- base_etudiants/main.cpp
+++ base_etudiants/main.cpp
@@ -1,6 +1,5 @@
 #include "common.h"
-#include "db.hpp"
-#include "queries.hpp"
+//#include "db.hpp"
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstdio>
@@ -8,70 +7,68 @@
 #include <csignal>
 
 #include <unistd.h>
-#include <string.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <algorithm>
-#include <iostream>
+#include <semaphore.h>
+#include <pthread.h>
 
 // Permet de définir un gestionnaire de signaux pour SIGPIPE,
 // ce qui évite une fermeture abrupte du programme à la réception
 // du signal (utile si vous avez des opérations de nettoyage à
 // faire avant de terminer le programme)
 #include <signal.h>
-int new_socket;
-database_t *db = new database_t;
 
-void *work(void *)
-{
-    std::cout << "thread created";
-    char *buffer = new char[1024]();
-    int lu;
-
-    while ((lu = read(new_socket, buffer, 1024)) > 0)
-    {
-        checked_wr(write(new_socket, buffer, lu));
-        std::cout << buffer;
-        FILE *ush = fopen("tmp/test.txt", "w");
-        if (ush == NULL)
-        {
-            puts("errorfile");
-        }
-        fflush(ush);
-        parse_and_execute(ush, db, buffer);
-    }
-}
 
 int main(int argc, char const *argv[])
 {
-    pthread_t cThread;
+  //  Permet que write() retourne 0 en cas de réception
+  //  du signal SIGPIPE.
+  signal(SIGPIPE, SIG_IGN);
 
-    db->path = argv[1];
-    db_load(db, db->path);
-    signal(SIGPIPE, SIG_IGN);
+  int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
 
-    int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(8080);
 
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+  // Conversion de string vers IPv4 ou IPv6 en binaire
+  inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+  checked(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
 
-    checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address)));
-    checked(listen(server_fd, 3));
+  char buffer[1024];
+  int longueur, i, ret;
 
-    size_t addrlen = sizeof(address);
-    new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
-    pthread_create(&cThread, NULL, work, NULL);
-    pthread_join(cThread, NULL);
-    close(server_fd);
-    close(new_socket);
-    return 0;
+  while (fgets(buffer, 1024, stdin) != NULL)
+  {
+    int i = strlen(buffer) - 1;
+    buffer[i] = '\0';
+    longueur = strlen(buffer) + 1;
+    printf("Envoi...\n");
+    checked_wr(write(sock, buffer, strlen(buffer) + 1));
+    i = 0;
+    while (i < longueur)
+    {
+      ret = read(sock, buffer, longueur - i);
+      if (ret <= 0)
+      {
+        if (ret < 0)
+          perror("read");
+        else
+          printf("Déconnexion du serveur.\n");
+        return 1;
+      }
+
+      i += ret;
+    }
+
+    printf("Recu : %s\n", buffer);
+  }
+
+  close(sock);
+  return 0;
 }
