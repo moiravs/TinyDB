e7a4a72f6cfe2441dc727506b39a04b8f52731bb
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-22 16:42:11 +0100

    deleted clientThread

--- base_etudiants/smalldb.cpp
+++ base_etudiants/smalldb.cpp
@@ -22,33 +22,6 @@
 // faire avant de terminer le programme)
 #include <signal.h>
 
-void *clientThread(void *args){
-  int client_request = *((int*)args);
-  int network_socket;
-
-  network_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-  struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(8989);
-
-  int connection_status = connect(network_socket, (struct sockaddr*)&server_address, sizeof(server_address));
-
-  if (connection_status < 0){  // check for connection error
-    puts("Error\n");
-    return 0;
-  }
-
-  printf("Connection established\n");
-  
-  send(network_socket, &client_request, sizeof(client_request), 0); // send data to the socket
-
-  close(network_socket);
-  pthread_exit(NULL);
-  return 0;
-}
-
 
 int main(int argc, char const *argv[])
 {
