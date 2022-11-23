55d466033e130d6165906668b11f20f6cc830fdb
andriusezerskis <andrius.ezerskis@ulb.be>
2022-11-23 16:54:32 +0100

    join threads added idk what to do

--- base_etudiants/smalldb.cpp
+++ base_etudiants/smalldb.cpp
@@ -145,5 +145,9 @@ int main(int argc, char *argv[])
         pthread_create(&tid, NULL, &func, (void *)&args);
         poolofthread.push_back(tid);
     }
+
+    for (auto &thread: poolofthread){
+        pthread_join(thread, NULL);
+    }
     return 0;
 }
